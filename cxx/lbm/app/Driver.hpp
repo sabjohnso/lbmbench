87535baa436ae208965a11f2163ef92462c18da4
Samuel B. Johnson <sabjohnso@yahoo.com>
2024-07-28 11:07:23 -0500

    :stone: [Driver]
    Problem:
    - Recompilation is slow.
    
    Solution:
    - Move inlinen `Driver` functions out-of-line.

--- cxx/lbm/app/Driver.hpp
+++ cxx/lbm/app/Driver.hpp
@@ -13,29 +13,8 @@ namespace lbm::app {
   public:
     Driver() = delete;
 
-    Driver(int argc, char **argv)
-        : runtime_config_{argc, argv}
-        , input_{runtime_config_.input()} {
-
-      switch (input_.kernel()) {
-      case Kernel::D2Q9:
-        switch (input_.float_type()) {
-        case Float_Type::FLOAT32:
-          pstate_ = make_unique<D2Q9::State<float32_t>>(input_);
-          break;
-        case Float_Type::FLOAT64:
-          pstate_ = make_unique<D2Q9::State<float64_t>>(input_);
-          break;
-        }
-        break;
-      }
-
-      for (size_type i = 0; i < input_.num_steps(); ++i) {
-        pstate_->step();
-      }
-    }
-
-    operator int() const { return 0; }
+    Driver(int argc, char **argv);
+    operator int() const;
 
   private:
     Runtime_Config runtime_config_;
