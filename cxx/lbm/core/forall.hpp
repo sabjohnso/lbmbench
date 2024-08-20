8ea45c283a8f4edbf2157e7364e84f32cb43fab7
Samuel B. Johnson <sabjohnso@yahoo.com>
2024-08-20 08:02:49 -0500

    :sparkles: Binary implementation of `forall`

--- cxx/lbm/core/forall.hpp
+++ cxx/lbm/core/forall.hpp
@@ -18,4 +18,22 @@ namespace lbm::core {
     }
     return func;
   }
+
+  /**
+   * @brief Function passing every iterator from two half-open ranges to
+   * an input binary function, which is return.
+   *
+   * @details This function is similar to the `std::for_each`, whith
+   * notable exception that it passes the iterator to the function,
+   * not the dereferenced value, and accepts to ranges.
+   */
+  template <class Input_Iter1, class Input_Iter2, class Binary_Function>
+  Unary_Function
+  forall(Input_Iter first1, Input_Iter last1, Input_Iter first2, Binary_Function func) {
+    while (first1 != last1) {
+      func(first1++, first2++);
+    }
+    return func;
+  }
+
 } // end of namespace lbm::core
